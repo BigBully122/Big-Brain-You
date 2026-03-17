61d5caca9b83f91bb716214be2c6c46d2998ad21
Faysel Alali <106882754+BigBully122@users.noreply.github.com>
2026-03-17 15:35:46 +0100

    Created a second map for the game play

--- big-brain-you-/scripts/home_menu.gd
+++ big-brain-you-/scripts/home_menu.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_playbtn_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/home_scene_map.tscn")
+
+
+func _on_quitbtn_pressed() -> void:
+	get_tree().change_scene_to_file("res://game_play_scene.tscn")
